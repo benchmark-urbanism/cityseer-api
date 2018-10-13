460876d672d6fd72ea094a6c2092581536fbf6df
gareth <garethsimons@me.com>
2018-10-13 17:22:55 +0100

    version bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from cityseer import centrality, networks, mixed_uses, accessibility
 
 setup (
     name = 'cityseer',
-    version = '0.1.8',
+    version = '0.1.9',
     packages=['cityseer'],
     description = 'Computational tools for urban analysis',
     url='https://github.com/cityseer/cityseer-api',
