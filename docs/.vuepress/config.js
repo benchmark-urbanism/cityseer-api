bd4eecb071001a17a7c78b254c7c2b99e03ab3a7
gareth <garethsimons@me.com>
2019-02-22 11:19:24 +0000

    need to load katex locally

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,27 @@ module.exports = {
   base: '/cityseer/',  // must match github pages publish URL
   title: 'Cityseer API Docs',
   description: 'Computational tools for urban analysis',
+  head: [
+    ['link', {
+      rel: 'stylesheet',
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.10.0/dist/katex.min.css',
+      integrity: 'sha384-9eLZqc9ds8eNjO3TmqPeYcDj8n+Qfa4nuSiGYa6DjLNcv9BtN69ZIulL9+8CqC9Y',
+      crossorigin: 'anonymous'
+    }],
+    ['script', {
+      defer: true,
+      src: 'https://cdn.jsdelivr.net/npm/katex@0.10.0/dist/katex.min.js',
+      integrity: 'sha384-K3vbOmF2BtaVai+Qk37uypf7VrgBubhQreNQe9aGsz9lB63dIFiQVlJbr92dw2Lx',
+      crossorigin: 'anonymous'
+    }],
+    // must include katex stylesheet otherwise double displayed MathML and HTML
+    ['script', {
+      defer: true,
+      src: 'https://cdn.jsdelivr.net/npm/katex@0.10.0/dist/contrib/auto-render.min.js',
+      integrity: 'sha384-kmZOZB5ObwgQnS/DuDg6TScgOiWWBiVt0plIRkZCmE6rDZGrEOQeHM5PcHi+nyqe',
+      crossorigin: 'anonymous'
+    }]
+  ],
   plugins: [],
   markdown: {
     lineNumbers: true,
