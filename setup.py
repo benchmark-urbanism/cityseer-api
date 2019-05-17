6e20c9f2d4efddffa74b906dec37d51170d4a14b
gareth <garethsimons@me.com>
2019-05-17 14:52:04 +0100

    Bumps version number.

--- setup.py
+++ setup.py
@@ -10,24 +10,12 @@ pip install --upgrade twine
 TESTING REPO: twine upload --repository-url https://test.pypi.org/legacy/ dist/*
 DOWNLOADING FROM TEST REPO: pip install --extra-index-url https://test.pypi.org/simple/ cityseer==0.7.6rc7
 OTHERWISE: twine upload dist/*
-
-For AOT: (not preferred):
--------------------------
-if compiling numba directly from file:
-cc.output_dir = os.path.abspath(os.path.join(os.pardir, 'compiled'))
-cc.verbose = True
-cc.compile()
-use numba.typeof to deduce signatures
-nest @njit decorator to help aot functions find each other, see https://stackoverflow.com/questions/49326937/error-when-compiling-a-numba-module-with-function-using-other-functions-inline
-
-To test against AOT compiled modules, run python setup.py test, which will copy compiled modules to working modules:
-then run pytest test, which now tests against compiled .so files...
 '''
 from setuptools import setup
 
 setup (
     name = 'cityseer',
-    version='0.8.9',
+    version='0.8.10',
     packages=['cityseer', 'cityseer.algos', 'cityseer.metrics', 'cityseer.util'],
     description = 'Computational tools for urban analysis',
     url='https://github.com/cityseer/cityseer-api',
@@ -48,13 +36,5 @@ setup (
         'tqdm',
         'matplotlib',
         'sklearn'
-    ],
-    # ext_package='cityseer.algos',  # NB -> sets output directory for extension modules
-    # some sort of issue with AOT precompilation - using njit for now...
-    ext_modules = [
-        # centrality.cc.distutils_extension(),
-        # data.cc.distutils_extension(),
-        # diversity.cc.distutils_extension(),
-        #checks.cc.distutils_extension()
     ]
 )
