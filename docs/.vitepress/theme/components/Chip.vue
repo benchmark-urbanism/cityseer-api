48510bf048b38071197655a93ec8e88a4abcab34
Gareth Simons <garethsimons@me.com>
2021-02-11 13:37:43 +0000

    migrates docs from vuepress to vitepress

--- docs/.vitepress/theme/components/Chip.vue
+++ docs/.vitepress/theme/components/Chip.vue
@@ -7,23 +7,20 @@
   </div>
 </template>
 
-<style lang="stylus">
+<style lang="postcss" scoped>
 
-  .chip-style
-    display inline-block
-    font-size 12px
-    height 20px
-    line-height 20px
-    border-radius 2px
-    padding 0 6px
-    color white
-    margin-right 5px
-    background-color $info
-    vertical-align top
-
-  &.important
-    background-color $secondary
-
+.chip-style {
+  display: inline-block;
+  font-size: 12px;
+  height: 20px;
+  line-height: 20px;
+  border-radius: 2px;
+  padding: 0 6px;
+  color: white;
+  margin-right: 5px;
+  background-color: #d32f2f;
+  vertical-align: top;
+}
 </style>
 
 <script>
---
+++ docs/.vitepress/theme/components/FuncSignature.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="sig-style">
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="postcss" scoped>
+
+.sig-style {
+  margin: 30px 0;
+  padding: 1.1em;
+  font-size: 18px;
+  font-family: monospace;
+  font-weight: bold;
+  color: #ffffff;
+  background-color: #2e2e2e;
+  border-radius: 10px;
+}
+
+/* necessary for when wrapping signatures with <pre></pre> tags for spacing */
+.sig-style pre {
+  margin: 0;
+  padding: 0;
+}
+</style>
