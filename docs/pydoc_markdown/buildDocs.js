ce16cbdddf2a0da9870e0624dc31dfcb7c9e5e0e
Gareth Simons <garethsimons@me.com>
2021-02-22 21:59:59 +0000

    cleans up pipeline for building docs

--- docs/pydoc_markdown/buildDocs.js
+++ docs/pydoc_markdown/buildDocs.js
@@ -2,18 +2,18 @@ const path = require('path')
 const { PythonShell } = require('python-shell')
 
 console.log('boo')
-console.log(path.resolve(__dirname, '.'))
+console.log(path.resolve(__dirname, '..'))
 
 const options = {
   mode: 'text',
   pythonPath: 'venv/bin/python',
-  pythonOptions: ['-u'],
+  pythonOptions: ['-u', '-m'],
   // make sure you use an absolute path for scriptPath
   scriptPath: path.resolve(__dirname, '.'),
   args: [],
 }
 
-PythonShell.run('docs/pydoc_markdown/pydoc_builder.py', options, function (err) {
+PythonShell.run('cityseer/docs/pydoc_markdown/pydoc_builder', options, function (err) {
   if (err) throw err
   console.log('finished')
 })
