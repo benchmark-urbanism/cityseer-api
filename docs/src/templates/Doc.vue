4448f2ef578b68f412bdbf7ce81c64f6110a3b4c
Gareth Simons <garethsimons@me.com>
2021-03-01 22:22:51 +0000

    switching docs to gridsome

---
+++ docs/src/templates/Doc.vue
@@ -0,0 +1,17 @@
+<template>
+  <Layout>
+    <p class="intro">
+      {{ $page.doc.excerpt }}
+    </p>
+    <VueRemarkContent />
+  </Layout>
+</template>
+
+<!-- Front-matter fields can be queried from GraphQL layer -->
+<page-query>
+query ($id: ID!) {
+  doc (id: $id) {
+    title
+  }
+}
+</page-query>
