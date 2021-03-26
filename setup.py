aafd5f2e1c09b8c8cb52ed902b19080c92f4e494
Gareth Simons <garethsimons@me.com>
2021-03-26 12:59:20 +0000

    Updates setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ pip install --upgrade setuptools wheel
 python setup.py sdist bdist_wheel
 pip install --upgrade twine
 TESTING REPO: twine upload --repository-url https://test.pypi.org/legacy/ dist/*
-DOWNLOADING FROM TEST REPO: pip install --extra-index-url https://test.pypi.org/simple/ cityseer==1.0.0a1
+DOWNLOADING FROM TEST REPO: pip install --extra-index-url https://test.pypi.org/simple/ cityseer==1.0.4
 OTHERWISE: twine upload dist/*
 '''
 
@@ -16,7 +16,7 @@ from setuptools import setup
 
 setup(
     name='cityseer',
-    version='1.0.3',
+    version='1.0.4',
     packages=['cityseer', 'cityseer.algos', 'cityseer.metrics', 'cityseer.tools'],
     description='Computational tools for urban analysis',
     url='https://github.com/benchmark-urbanism/cityseer-api',
@@ -26,16 +26,16 @@ setup(
         "Source Code": "https://github.com/benchmark-urbanism/cityseer-api",
     },
     author='Gareth Simons',
-    author_email='gareth@cityseer.io',
-    license='Apache 2.0 + "Commons Clause" License Condition v1.0',
+    author_email='info@benchmarkurbanism.com',
+    license='GNU GPLv3',
     install_requires=[
-        'numpy',
-        'numba',
-        'utm',
-        'shapely>=1.7.0',
-        'networkx>=2.4',
-        'tqdm',
         'matplotlib',
-        'sklearn'
+        'networkx>=2.4',
+        'numba>=0.53',
+        'numpy',
+        'shapely >= 1.7.0',
+        'sklearn',
+        'tqdm',
+        'utm'
     ]
 )
