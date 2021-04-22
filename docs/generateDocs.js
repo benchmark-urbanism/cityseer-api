b827e79bf1d6bc0801a9ac3036999be640ad7d86
Gareth Simons <garethsimons@me.com>
2021-04-22 14:55:12 +0100

    abstracts yapper

--- docs/generateDocs.js
+++ docs/generateDocs.js
@@ -6,13 +6,18 @@ const options = {
   pythonOptions: ['-u'],
   // make sure you use an absolute path for scriptPath
   scriptPath: path.resolve(__dirname),
-  args: [],
+  args: ['--config', './.yap_config.yaml'],
 }
+PythonShell.runString('python -m site', null, function (err, result) {
+  if (err) throw err
+  console.log(result)
+})
 
 if (!process.env.CI) {
+  options['scriptPath'] = path.resolve(__dirname, '../venv/lib/python3.9/site-packages/yapper')
   options['pythonPath'] = path.resolve(__dirname, '../venv/bin/python')
 }
 
-PythonShell.run('simple_docstring_parser.py', options, function (err) {
+PythonShell.run('__init__.py', options, function (err) {
   if (err) throw err
 })
