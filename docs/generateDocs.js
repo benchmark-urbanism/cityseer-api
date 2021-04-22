dd9657e4ef3bb0aaee041269445d000fd5727f58
Gareth Simons <garethsimons@me.com>
2021-04-22 15:36:39 +0100

    abstracts yapper

--- docs/generateDocs.js
+++ docs/generateDocs.js
@@ -4,18 +4,9 @@ const { PythonShell } = require('python-shell')
 const options = {
   mode: 'text',
   pythonOptions: ['-u'],
-  // make sure you use an absolute path for scriptPath
-  scriptPath: path.resolve(__dirname),
-  args: ['--config', './.yap_config.yaml'],
-}
-PythonShell.runString('python -m site', null, function (err, result) {
-  if (err) throw err
-  console.log(result)
-})
-
-if (!process.env.CI) {
-  options['scriptPath'] = path.resolve(__dirname, '../venv/lib/python3.9/site-packages/yapper')
-  options['pythonPath'] = path.resolve(__dirname, '../venv/bin/python')
+  pythonPath: path.resolve(__dirname, '../venv/bin/python'),
+  scriptPath: path.resolve(__dirname, '../venv/lib/python3.9/site-packages/yapper'),
+  args: ['--config', '../.yap_config.yaml'],
 }
 
 PythonShell.run('__init__.py', options, function (err) {
