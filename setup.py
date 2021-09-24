45c74eef5d0c8fc3b2fa35f6025fc3c6f042b2bd
Gareth Simons <garethsimons@me.com>
2021-09-24 11:44:10 +0100

    Adds requests library to requirements

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='cityseer',
-    version='1.1.5',
+    version='1.1.6',
     packages=['cityseer', 'cityseer.algos', 'cityseer.metrics', 'cityseer.tools'],
     description='Computational tools for urban analysis',
     url='https://github.com/benchmark-urbanism/cityseer-api',
@@ -24,6 +24,7 @@ setup(
         'numba>=0.53',
         'numpy',
         'pytest',
+        'requests',
         'shapely>=1.7',
         'sklearn',
         'tqdm',
