17dc057f1c4f25fb4e082feac7f395a3bf0d82d4
Gareth Simons <garethsimons@me.com>
2021-10-17 09:09:30 +0100

    Dev version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='cityseer',
-    version='1.1.7b3',
+    version='1.1.7b4',
     packages=['cityseer', 'cityseer.algos', 'cityseer.metrics', 'cityseer.tools'],
     description='Computational tools for urban analysis',
     url='https://github.com/benchmark-urbanism/cityseer-api',
