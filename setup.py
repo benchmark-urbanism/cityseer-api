016d75f82751ef7d53c1c43caae57649d14f2378
Gareth Simons <garethsimons@me.com>
2021-11-25 16:35:46 +0000

    Trying NumbaProgress

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 """
-DOWNLOADING FROM TEST REPO: pip install --extra-index-url https://test.pypi.org/simple/ cityseer==1.1.7b8
+DOWNLOADING FROM TEST REPO: pip install --extra-index-url https://test.pypi.org/simple/ cityseer==1.1.7b9
 """
 
 from setuptools import setup
 
 setup(
     name='cityseer',
-    version='1.1.7b8',
+    version='1.1.7b9',
     packages=['cityseer', 'cityseer.algos', 'cityseer.metrics', 'cityseer.tools'],
     description='Computational tools for urban analysis',
     url='https://github.com/benchmark-urbanism/cityseer-api',
@@ -22,6 +22,7 @@ setup(
         'matplotlib',
         'networkx>=2.4',
         'numba>=0.53',
+        'numba-progress',
         'numpy',
         'pytest',
         'requests',
