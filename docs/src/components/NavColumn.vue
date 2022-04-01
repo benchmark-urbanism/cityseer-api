17371a6c00eea68b1749c0ea9c6f4a538896c351
Gareth Simons <garethsimons@me.com>
2022-04-01 18:24:02 +0100

    astro

--- docs/src/components/NavColumn.vue
+++ docs/src/components/NavColumn.vue
@@ -13,16 +13,15 @@ nav#nav-tree
     div(v-show='nav.headerInfo.length').flex.flex-col.items-end.py-2
       a.nested-link(
         v-for='header in nav.headerInfo'
-        :key='header.path'
         :title='header.title'
-        :href='header.path'
+        :href='header.targetPath'
+        :id='header.headerId'
       ) {{ header.title }}
 </template>
 
 <script setup>
-import { useIntersectionObserver, useThrottleFn, useMediaQuery } from '@vueuse/core'
-import { computed, ref } from 'vue'
-import { Rocket, Github } from '@vicons/fa'
+import { useIntersectionObserver } from '@vueuse/core'
+import { onMounted, reactive, ref } from 'vue'
 
 const props = defineProps({
   navPaths: {
@@ -35,85 +34,61 @@ const props = defineProps({
   },
 })
 const contentCol = document.getElementById('content-col')
-const headers = contentCol.querySelectorAll('h1, h2')
-const navTree = computed(() => {
-  const tree = []
+const headers = contentCol.querySelectorAll('h1, h2, h3')
+const navTree = reactive([])
+let activeNavElems = []
+onMounted(() => {
   props.navPaths.forEach((path) => {
     const isActive = path === props.currentPath
-    console.log(isActive)
     const headerInfo = []
     if (isActive) {
       headers.forEach((header) => {
+        const headerId = `head-${header.id}`
+        const targetPath = `#${header.id}`
         headerInfo.push({
           title: header.outerText,
-          level: header.localName,
-          id: header.id,
-          path: `#${header.id}`,
+          headerId,
+          targetPath,
+        })
+        useIntersectionObserver(header, (entries) => {
+          const entry = entries.pop()
+          const navElem = document.getElementById(headerId)
+          const targetIdx = activeNavElems.findIndex((el) => el.name === navElem.id)
+          // if heading visible, add to list of nav targets if properly new
+          if (entry.isIntersecting && targetIdx === -1) {
+            navElem.classList.add('nested-link-visible')
+            activeNavElems.push({
+              name: navElem.id,
+              targetElem: navElem,
+              visible: true,
+            })
+          } else if (!entry.isIntersecting && targetIdx >= 0) {
+            // if target no longer visible, leave for now, but update visibility accordingly
+            activeNavElems[targetIdx].visible = false
+          }
+          // filter out still visible targets
+          const stillVisible = activeNavElems.filter((el) => el.visible)
+          // if visible elements - use only those
+          if (stillVisible.length) {
+            activeNavElems.forEach((el) => {
+              if (!el.visible) el.targetElem.classList.remove('nested-link-visible')
+            })
+            // turn off the rest and forget
+            activeNavElems = stillVisible
+          } else if (targetIdx >= 0) {
+            // otherwise, keep the current element for now
+            activeNavElems = [activeNavElems[targetIdx]]
+          }
         })
       })
     }
-    tree.push({
+    navTree.push({
       active: isActive,
       path,
       headerInfo,
     })
   })
-  return tree
 })
-
-const isWide = useMediaQuery('(min-width: 750px)')
-const isTall = useMediaQuery('(min-height: 620px)')
-
-const laggedElem = null
-const fillerElem = null
-const scrollToTopVisible = false
-const scrollTo = (targetEl) => {
-  window.scroll(targetEl)
-}
-const onElementObserved = (entries) => {
-  if (!this.observerReady) return
-  if (!('navView' in this.$refs) || !this.$refs.navView) return
-  // target class
-  const targetClass = 'nested-link-active'
-  // clean up fillers - these will be re-enabled if necessary
-  if (this.fillerElem) {
-    this.fillerElem.classList.remove(targetClass)
-    this.fillerElem = null
-  }
-  // process state changes
-  entries.forEach((entry) => {
-    const elemId = this.makeElemId(entry.target.id, true)
-    const elem = this.$refs.navView.querySelector(elemId)
-    if (!elem) return
-    // add new
-    if (entry.isIntersecting) {
-      elem.classList.add(targetClass)
-      // refresh lagged element to most recent
-      this.laggedElem = elem
-      // and update state
-      this.h2Elems[elemId] = true
-    } else {
-      // remove old
-      elem.classList.remove(targetClass)
-      // and update state
-      this.h2Elems[elemId] = false
-    }
-  })
-  // count the existing active instances - an object is necessary for tracking state
-  const countActive = Object.values(this.h2Elems).reduce((aggState, currentState) => {
-    if (currentState) return (aggState += 1)
-    return aggState
-  }, 0)
-  // if there are no existing or new active elements, use the trailing element as a filler
-  if (!countActive && this.laggedElem) {
-    this.fillerElem = this.laggedElem
-    this.fillerElem.classList.add(targetClass)
-  }
-}
-const observer = new IntersectionObserver(onElementObserved)
-// this.$refs.routerView.querySelectorAll('h2').forEach((el) => {
-//   this.observer.observe(el)
-// })
 </script>
 
 <style lang="postcss" scoped>
@@ -132,6 +107,9 @@ const observer = new IntersectionObserver(onElementObserved)
   &:active {
     @apply border-r-2;
   }
+}
+.nested-link-visible {
+  @apply border-r-2;
 }
 @media only screen and (max-width: 958px) {
   .nav-link {
