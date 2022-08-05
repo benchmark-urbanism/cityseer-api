d9f91015be29118dca6806ff62f81e52fa161bd0
Gareth Simons <garethsimons@me.com>
2022-08-05 08:44:43 +0100

    updates docs for new io module

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,3 +1,4 @@
+import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import vue from '@astrojs/vue'
@@ -123,8 +124,6 @@ export default defineConfig({
       },
     }),
     sitemap(),
+    mdx(),
   ],
-  legacy: {
-    astroFlavoredMarkdown: true,
-  }
 })
