f9bdb9a3de86109a590427506ed7bd043c1e935b
Gareth Simons <garethsimons@me.com>
2023-04-26 20:10:31 +0100

    ongoing rust exploration

--- tests/test_temp.py
+++ tests/test_temp.py
@@ -2,4 +2,6 @@ import cityseer
 
 
 def test_rust():
-    print(cityseer.rustalgos.multiply(1, 2))
+    print(cityseer)
+    # data_entry = cityseer.structures.DataEntry(1, 2)
+    # print(data_entry)
