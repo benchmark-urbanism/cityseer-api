9741f0a00d89ff26f8ccee1e2ca329d22feea420
Gareth Simons <garethsimons@me.com>
2023-05-01 21:19:35 +0100

    proof of concept

---
+++ tests/test_temp.py
@@ -0,0 +1,15 @@
+from cityseer.metrics import networks
+from cityseer.tools import graphs, mock
+
+G_primal = mock.mock_graph()
+G_primal = graphs.nx_simple_geoms(G_primal)
+# load the test graph
+_nodes_gdf, _edges_gdf, network_structure = graphs.network_structure_from_nx(G_primal, 3395)
+# needs a large enough beta so that distance thresholds aren't encountered
+distances, betas = networks.pair_distances_betas(distances=[5000])
+
+network_structure.local_node_centrality_shortest(
+    distances,
+    betas,
+    True,
+)
