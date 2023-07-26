8deaad1e039ae8664c65410be4edb8bcf4fed798
Gareth Simons <garethsimons@me.com>
2023-07-26 22:43:58 +0100

    adds mixed use measures

--- src/data.rs
+++ src/data.rs
@@ -1,12 +1,13 @@
 use crate::common::MetricResult;
 use crate::common::{clip_wts_curve, clipped_beta_wt, pair_distances_and_betas, Coord};
+use crate::diversity;
 use crate::graph::NetworkStructure;
 use indicatif::{ProgressBar, ProgressDrawTarget};
 use numpy::PyArray1;
 use pyo3::exceptions;
 use pyo3::prelude::*;
 use rayon::prelude::*;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::sync::atomic::Ordering;
 
 #[pyclass]
@@ -15,6 +16,24 @@ struct AccessibilityResult {
     weighted: HashMap<u32, Py<PyArray1<f32>>>,
     #[pyo3(get)]
     unweighted: HashMap<u32, Py<PyArray1<f32>>>,
+}
+#[pyclass]
+struct MixedUsesHillResult {
+    #[pyo3(get)]
+    hill: HashMap<u32, HashMap<u32, Py<PyArray1<f32>>>>,
+    #[pyo3(get)]
+    hill_weighted: HashMap<u32, HashMap<u32, Py<PyArray1<f32>>>>,
+}
+#[pyclass]
+struct MixedUsesOtherResult {
+    #[pyo3(get)]
+    shannon: HashMap<u32, Py<PyArray1<f32>>>,
+    #[pyo3(get)]
+    gini: HashMap<u32, Py<PyArray1<f32>>>,
+}
+struct ClassesState {
+    count: u32,
+    nearest: f32,
 }
 #[pyclass]
 #[derive(Clone)]
@@ -294,10 +313,10 @@ impl DataMap {
                 return;
             }
             /*
-            # generate the reachable classes and their respective distances
-            # these are non-unique - i.e. simply the class of each data point within the maximum distance
-            # the aggregate_to_src_idx method will choose the closer direction of approach to a data point
-            # from the nearest or next-nearest network node
+            generate the reachable classes and their respective distances
+            these are non-unique - i.e. simply the class of each data point within the maximum distance
+            the aggregate_to_src_idx method will choose the closer direction of approach to a data point
+            from the nearest or next-nearest network node
             */
             let reachable_entries = self.aggregate_to_src_idx(
                 *netw_src_idx,
@@ -325,6 +344,7 @@ impl DataMap {
                 }
             }
         });
+        pbar.finish();
         // unpack
         let mut accessibilities: HashMap<String, AccessibilityResult> = HashMap::new();
         for acc_key in accessibility_keys.iter() {
@@ -337,5 +357,219 @@ impl DataMap {
             );
         }
         Ok(accessibilities)
+    }
+    fn mixed_uses(
+        &self,
+        network_structure: &NetworkStructure,
+        landuses_map: HashMap<String, String>,
+        mixed_uses_hill: Option<bool>,
+        mixed_uses_other: Option<bool>,
+        distances: Option<Vec<u32>>,
+        betas: Option<Vec<f32>>,
+        angular: Option<bool>,
+        spatial_tolerance: Option<u32>,
+        min_threshold_wt: Option<f32>,
+        jitter_scale: Option<f32>,
+        pbar_disabled: Option<bool>,
+    ) -> PyResult<(Option<MixedUsesHillResult>, Option<MixedUsesOtherResult>)> {
+        let (distances, betas) = pair_distances_and_betas(distances, betas, min_threshold_wt)?;
+        let max_dist: u32 = distances.iter().max().unwrap().clone();
+        if landuses_map.len() != self.count() {
+            return Err(exceptions::PyValueError::new_err(
+                "The number of landuse encodings must match the number of data points",
+            ));
+        }
+        let mixed_uses_hill = mixed_uses_hill.unwrap_or(false);
+        let mixed_uses_other = mixed_uses_other.unwrap_or(false);
+        if !mixed_uses_hill && !mixed_uses_other {
+            return Err(exceptions::PyValueError::new_err(
+                "Either or both closeness and mixed-use flags is required, but both parameters are False.",
+            ));
+        }
+        let spatial_tolerance = spatial_tolerance.unwrap_or(0);
+        let max_curve_wts = clip_wts_curve(distances.clone(), betas.clone(), spatial_tolerance)?;
+        // metrics
+        let hill_mu: HashMap<u32, MetricResult> = vec![0, 1, 2]
+            .into_iter()
+            .map(|q| {
+                (
+                    q,
+                    MetricResult::new(distances.clone(), network_structure.node_count()),
+                )
+            })
+            .collect();
+        let hill_wt_mu: HashMap<u32, MetricResult> = vec![0, 1, 2]
+            .into_iter()
+            .map(|q| {
+                (
+                    q,
+                    MetricResult::new(distances.clone(), network_structure.node_count()),
+                )
+            })
+            .collect();
+        let shannon_mu = MetricResult::new(distances.clone(), network_structure.node_count());
+        let gini_mu = MetricResult::new(distances.clone(), network_structure.node_count());
+        // prepare unique landuse classes
+        let mut classes_uniq: HashSet<String> = HashSet::new();
+        for cl_code in landuses_map.values() {
+            classes_uniq.insert(cl_code.clone());
+        }
+        // indices
+        let node_indices: Vec<usize> = network_structure.node_indices();
+        // pbar
+        let pbar_disabled = pbar_disabled.unwrap_or(false);
+        let pbar = ProgressBar::new(node_indices.len() as u64)
+            .with_message("Computing shortest path node centrality");
+        if pbar_disabled {
+            pbar.set_draw_target(ProgressDrawTarget::hidden())
+        }
+        // iter
+        node_indices.par_iter().for_each(|netw_src_idx| {
+            pbar.inc(1);
+            // skip if not live
+            if !network_structure.is_node_live(*netw_src_idx) {
+                return;
+            }
+            // reachable
+            let reachable_entries = self.aggregate_to_src_idx(
+                *netw_src_idx,
+                network_structure,
+                max_dist,
+                jitter_scale,
+                angular,
+            );
+            // counts and nearest instance of each class type by distance threshold
+            let mut classes: HashMap<u32, HashMap<String, ClassesState>> = HashMap::new();
+            for &dist_key in &distances {
+                let mut temp: HashMap<String, ClassesState> = HashMap::new();
+                for cl_code in &classes_uniq {
+                    temp.insert(
+                        cl_code.clone(),
+                        ClassesState {
+                            count: 0,
+                            nearest: 0.0,
+                        },
+                    );
+                }
+                classes.insert(dist_key, temp);
+            }
+            // iterate reachables to calculate reachable class counts and the nearest of each specimen
+            for (data_key, data_dist) in reachable_entries {
+                // get the class category
+                let cl_code = landuses_map[&data_key].clone();
+                // iterate the distance dimensions
+                for d in distances.iter() {
+                    // increment class counts at respective distances if the distance is less than current dist
+                    if data_dist <= *d as f32 {
+                        let class_state = classes
+                            .get_mut(&d)
+                            .unwrap()
+                            .get_mut(&cl_code.to_string())
+                            .unwrap();
+                        class_state.count += 1;
+                        // if distance is nearer, update the nearest distance vector too
+                        if data_dist < class_state.nearest {
+                            class_state.nearest = data_dist;
+                        }
+                    }
+                }
+            }
+            // iterate the distance dimensions
+            for i in 0..distances.len() {
+                let d = distances[i];
+                let b = betas[i];
+                let mcw = max_curve_wts[i];
+                // extract counts and nearest
+                let mut counts: Vec<u32> = Vec::new();
+                let mut nearest: Vec<f32> = Vec::new();
+                // Iterating over the classes HashMap
+                for classes_state in classes[&d].values() {
+                    counts.push(classes_state.count);
+                    nearest.push(classes_state.nearest);
+                }
+                // hill
+                if mixed_uses_hill {
+                    hill_mu[&0].metric[i][*netw_src_idx].fetch_add(
+                        diversity::hill_diversity(counts.clone(), 0.0).unwrap(),
+                        Ordering::Relaxed,
+                    );
+                    hill_mu[&1].metric[i][*netw_src_idx].fetch_add(
+                        diversity::hill_diversity(counts.clone(), 1.0).unwrap(),
+                        Ordering::Relaxed,
+                    );
+                    hill_mu[&2].metric[i][*netw_src_idx].fetch_add(
+                        diversity::hill_diversity(counts.clone(), 2.0).unwrap(),
+                        Ordering::Relaxed,
+                    );
+                    hill_wt_mu[&0].metric[i][*netw_src_idx].fetch_add(
+                        diversity::hill_diversity_branch_distance_wt(
+                            counts.clone(),
+                            nearest.clone(),
+                            0.0,
+                            b,
+                            mcw,
+                        )
+                        .unwrap(),
+                        Ordering::Relaxed,
+                    );
+                    hill_wt_mu[&1].metric[i][*netw_src_idx].fetch_add(
+                        diversity::hill_diversity_branch_distance_wt(
+                            counts.clone(),
+                            nearest.clone(),
+                            1.0,
+                            b,
+                            mcw,
+                        )
+                        .unwrap(),
+                        Ordering::Relaxed,
+                    );
+                    hill_wt_mu[&2].metric[i][*netw_src_idx].fetch_add(
+                        diversity::hill_diversity_branch_distance_wt(
+                            counts.clone(),
+                            nearest.clone(),
+                            2.0,
+                            b,
+                            mcw,
+                        )
+                        .unwrap(),
+                        Ordering::Relaxed,
+                    );
+                }
+                if mixed_uses_other {
+                    shannon_mu.metric[i][*netw_src_idx].fetch_add(
+                        diversity::shannon_diversity(counts.clone()).unwrap(),
+                        Ordering::Relaxed,
+                    );
+                    gini_mu.metric[i][*netw_src_idx].fetch_add(
+                        diversity::gini_simpson_diversity(counts.clone()).unwrap(),
+                        Ordering::Relaxed,
+                    );
+                }
+            }
+        });
+        pbar.finish();
+        let mu_hill_result: Option<MixedUsesHillResult> = if mixed_uses_hill {
+            let mut hill: HashMap<u32, HashMap<u32, Py<PyArray1<f32>>>> = HashMap::new();
+            let mut hill_weighted: HashMap<u32, HashMap<u32, Py<PyArray1<f32>>>> = HashMap::new();
+            for q_key in vec![0, 1, 2].iter() {
+                hill.insert(q_key.clone(), hill_mu[q_key].load());
+                hill_weighted.insert(q_key.clone(), hill_mu[q_key].load());
+            }
+            Some(MixedUsesHillResult {
+                hill,
+                hill_weighted,
+            })
+        } else {
+            None
+        };
+        let mu_other_result: Option<MixedUsesOtherResult> = if mixed_uses_other {
+            Some(MixedUsesOtherResult {
+                shannon: shannon_mu.load(),
+                gini: gini_mu.load(),
+            })
+        } else {
+            None
+        };
+        Ok((mu_hill_result, mu_other_result))
     }
 }
