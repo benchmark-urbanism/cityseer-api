726653e58a4608acd79ce9b614f1215806112612
Gareth Simons <garethsimons@me.com>
2024-11-25 14:21:33 +0000

    remove parking lots

--- pysrc/cityseer/tools/io.py
+++ pysrc/cityseer/tools/io.py
@@ -395,10 +395,21 @@ def osm_graph_from_poly(
         )
         plaza_area_gdf = _extract_gdf(plazas_gdf)
         plaza_area_gdf = plaza_area_gdf.to_crs(to_crs_code)
+        # parking
+        parking_gdf = ox.features_from_polygon(
+            geom_wgs,
+            tags={
+                "amenity": ["parking"],
+            },
+        )
+        parking_area_gdf = _extract_gdf(parking_gdf)
+        parking_area_gdf = parking_area_gdf.to_crs(to_crs_code)
         # use STR Tree for performance
         parks_buff_str_tree = STRtree(park_area_gdf.buffer(5).geometry.to_list())
         plaza_str_tree = STRtree(plaza_area_gdf.geometry.to_list())
+        parking_str_tree = STRtree(parking_area_gdf.geometry.to_list())
         # iter edges to find edges for marking
+        remove_edges = []
         for start_node_key, end_node_key, edge_key, edge_data in tqdm(
             graph_crs.edges(keys=True, data=True), total=graph_crs.number_of_edges()
         ):
@@ -421,7 +432,12 @@ def osm_graph_from_poly(
                 if len(itx):
                     idx = graph_crs[start_node_key][end_node_key][edge_key]["highways"].index("service")
                     graph_crs[start_node_key][end_node_key][edge_key]["highways"][idx] = "service_green"
-        #
+                # remove parking service roads
+                itx = parking_str_tree.query(edge_geom, predicate="intersects")
+                if len(itx):
+                    remove_edges.append((start_node_key, end_node_key, edge_key))
+        graph_crs.remove_edges_from(remove_edges)
+        # remove disconnected components
         graph_crs = graphs.nx_remove_dangling_nodes(graph_crs, despine=0, remove_disconnected=remove_disconnected)
         # clean by highway types - leave motorway and trunk as is
         for dist, tags, simplify_line_angles in (
